74bff97e942d1ba910420ecdd9127079cc854769
Aryan kamboj <aryankammboz@email.com>
2024-08-16 11:03:13 +0530

    new

--- controllers/codeCrudsade.js
+++ controllers/codeCrudsade.js
@@ -2,18 +2,18 @@ const Participant = require("../model/codeCrusade");
 
 const addParticipant = async (req, res) => {
   try {
-    const { email, fullname, admissionNumber, branch, contactNumber } =
-      req.body;
+    const { email, fullname, admissionNumber, branch, contactNumber } = req.body;
 
+    // Use await and findOne to check if the participant already exists
+    const existingParticipant = await Participant.findOne({ email });
 
-      const parti = Participant.find({email})
+    if (existingParticipant) {
+      res.status(400).send("Already registered!");
+      console.log("User registered already");
+      return; // Return to prevent adding a new participant
+    }
 
-      if(parti){
-        res.status(500).send("Already registered!")
-        console.log('user registered already')
-        return -1;
-      }
-
+    // Create and save the new participant
     const participant = new Participant({
       email,
       fullname,
@@ -23,7 +23,7 @@ const addParticipant = async (req, res) => {
     });
 
     await participant.save();
-    res.send("Participant Added Successfully"); 
+    res.send("Participant Added Successfully");
   } catch (e) {
     console.log(e);
     res.status(500).send("Server Error");
